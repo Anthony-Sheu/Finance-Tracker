607ac0f1dfcf926262c638731b5100b1641d5d37
Anthony Sheu <asheu@students.cs.ubc.ca>
2024-10-07 22:31:26 -0700

    Added new methods and rearranged the ordering to make it easier to see

--- src/main/ui/Menu.java
+++ src/main/ui/Menu.java
@@ -3,6 +3,7 @@ package ui;
 import java.util.*;
 
 import org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.IntegerConversion;
+import org.junit.platform.engine.discovery.PackageNameFilter;
 
 import model.Account;
 import model.Banks;
@@ -22,24 +23,35 @@ public class Menu {
     private Tracker tracker;
     private Transaction transaction;
 
+    //EFFECTS: runs start application
     public Menu(){
         start();
     }
 
+    // MODIFIES: this
+    //EFFECTS: takes in user input
     public void start() {
         int option = 0;
         init();
         while (true) {
             option = mainMenu();
-            if (option == 7) {
+            if (option == 5) {
                 break;
+            } else if (option == 1) {
+                transactionsProcess();
+            } else if (option == 2) {
+                bankAccountsProcess();
+            } else if (option == 3) {
+                creditCardProcess();
             } else {
-                process(option);
+                transferProcess();
             }
         }
         System.out.println("Goodbye!");
     }
 
+    // MODIFIES: this
+    // EFFECTS: initializes and prompts user to add bank info
     public void init() {
         bank = new Banks();
         category = new Categories();
@@ -50,41 +62,48 @@ public class Menu {
         addBank();
     }
 
+    // EFFECTS: prompts user input from main menu
     public int mainMenu() {
         System.out.println("======================MAIN MENU======================");
-        System.out.println("Would you like to (enter the number):");
-        System.out.println("1. Add a transaction");
-        System.out.println("2. See all transactions");
-        System.out.println("3. See all transactions in a given month");
-        System.out.println("4. See all transactions by an expense category");
-        System.out.println("5. Add new bank account");
-        System.out.println("6. See bank account balances");
-        System.out.println("7. Quit");
+        System.out.println("What would you like to view/edit? (enter the number):");
+        System.out.println("1. Transactions");
+        System.out.println("2. Bank accounts");
+        System.out.println("3. Credit card");
+        System.out.println("4. Transfer between accounts");
+        System.out.println("5. Quit");
         String command = input.nextLine();
         return Integer.parseInt(command);
     }
 
-    public void process(int num) {
-        if(num == 1) {
+    // MODIFIES: this
+    // EFFECTS: prompts user input from transactions menu
+    public void transactionsProcess() {
+        System.out.println("====================TRANSACTIONS=====================");
+        System.out.println("What would you like to do? (enter the number)");
+        System.out.println("1. Add a transaction");
+        System.out.println("2. See all transactions");
+        System.out.println("3. See all transactions in a given month/year");
+        System.out.println("4. See all transactions by an expense category");
+        System.out.println("5. Record a refund");
+        System.out.println("6. Return to main menu");
+        int command = Integer.parseInt(input.nextLine());
+        if (command == 1) {
             addTransaction();
-        } else if (num == 2) {
+        } else if (command == 2) {
             showTransaction(tracker.getTracker());
-        } else if (num == 3) {
+        } else if (command == 3) {
             showMonthTransaction();
-        } else if (num == 4) {
+        } else if (command == 4) {
             showExpenseTransaction();
-        } else if (num == 5) {
-            addBank();
-        } else {
-            showBank();
+        } else if (command == 5) {
+            refund();
         }
     }
 
-    public void enter() {
-        System.out.println("Press enter to continue");
-        input.nextLine();
-    }
-
+    // REQUIRES: amount >= 0
+    // MODIFIES: this
+    // EFFECTS: creates a new transaction and adds
+    // it to the tracker 
     public void addTransaction() {
         String command;
         int month;
@@ -113,6 +132,65 @@ public class Menu {
         updateBank(account, amount);
     }
 
+    // EFFECTS: prints out all transactions given
+    public void showTransaction(List<Transaction> show) {
+        System.out.println("=====================================================");
+        if (show.size() == 0) {
+            System.out.println("There are currently no transactions.");
+        } else {
+            for(int i = 0; i < show.size(); i++) {
+                System.out.print(Integer.toString(i+1)+". ");
+                System.out.println(show.get(i).printTransaction());
+            }
+        }
+        enter();
+    }
+
+    // EFFECTS: finds all transactions under an expense category 
+    // and prints it
+    public void showExpenseTransaction() {
+        System.out.println("=====================================================");
+        System.out.println("What expense category would you like to view?");
+        String expense = input.nextLine();
+        showTransaction(tracker.sortExpense(expense));
+    }
+
+    // EFFECTS: finds all transactions within a certain month/year
+    // and prints it
+    public void showMonthTransaction() {
+        System.out.println("=====================================================");
+        System.out.println("What year would you like to view?");
+        int year = Integer.parseInt(input.nextLine());
+        System.out.println("What month (in numbers)?");
+        int month = Integer.parseInt(input.nextLine());
+        showTransaction(tracker.sortMonth(month, year));
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes a refund by returning the amount back to
+    // the proper account
+    public void refund() {
+        System.out.println("=====================================================");
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prompts user input in bank accounts menu
+    public void bankAccountsProcess() {
+        System.out.println("====================BANK ACCOUNTS====================");
+        System.out.println("What would you like to do? (enter the number)");
+        System.out.println("1. Add new bank account");
+        System.out.println("2. See bank account balances");
+        System.out.println("3. Return to main menu");
+        int command = Integer.parseInt(input.nextLine());
+        if (command == 1) {
+            addBank();
+        } else if (command == 2) {
+            showBank();
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates a bank account with new amount
     public void updateBank(String info, double amount) {
         String[] accountSplit = info.split(" ");
         String name = accountSplit[0];
@@ -127,35 +205,8 @@ public class Menu {
         }
     }
 
-    public void showTransaction(List<Transaction> show) {
-        System.out.println("=====================================================");
-        if (show.size() == 0) {
-            System.out.println("There are currently no transactions.");
-        } else {
-            for(int i = 0; i < show.size(); i++) {
-                System.out.print(Integer.toString(i+1)+". ");
-                System.out.println(show.get(i).printTransaction());
-            }
-        }
-        enter();
-    }
-
-    public void showMonthTransaction() {
-        System.out.println("=====================================================");
-        System.out.println("What year would you like to view?");
-        int year = Integer.parseInt(input.nextLine());
-        System.out.println("What month (in numbers)?");
-        int month = Integer.parseInt(input.nextLine());
-        showTransaction(tracker.sortMonth(month, year));
-    }
-
-    public void showExpenseTransaction() {
-        System.out.println("=====================================================");
-        System.out.println("What expense category would you like to view?");
-        String expense = input.nextLine();
-        showTransaction(tracker.sortExpense(expense));
-    }
-
+    // MODIFIES: this
+    // EFFECTS: adds a new bank account
     public void addBank() {
         System.out.println("=====================================================");
         System.out.println("What is the name of your banking institution?");
@@ -171,14 +222,65 @@ public class Menu {
         account = new Account(cheq, save, cred, name, credLim);
         bank.newAccount(account);
     }
-    
+
+    // EFFECTS: prints out all banking information
     public void showBank() {
         System.out.println("=====================================================");
         System.out.println("Your banking information");
         for(Account acc : bank.getBank()) {
             System.out.println(acc.printAccount());
+            System.out.println();
         }
         enter();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prompts user input from credit cards menu
+    public void creditCardProcess() {
+        System.out.println("====================CREDIT CARDS=====================");
+        System.out.println("What would you like to do? (enter the number)");
+        System.out.println("1. Pay credit card");
+        System.out.println("2. Change credit card limit");
+        System.out.println("3. Return to main menu");
+        int command = Integer.parseInt(input.nextLine());
+        if (command == 1) {
+            payCreditCard();
+        } else if (command == 2) {
+            changeCreditLimit();
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: pays credit card by specified amount
+    public void payCreditCard() {}
+
+    // MODIFIES: this
+    // EFFECTS: changes credit card limit on a certain credit card
+    public void changeCreditLimit() {}
+
+    // MODIFIES: this
+    // EFFECTS: prompts user input from transfer menu
+    public void transferProcess() {
+        System.out.println("======================TRANSFER=======================");
+        System.out.println("1. Transfer between accounts");
+        System.out.println("2. return to main menu");
+        int command = Integer.parseInt(input.nextLine());
+        if (command == 1) {
+            transfer();
+        }
+
+    }
+
+    // REQUIRES: amount >= 0
+    // MODIFIES: this
+    // EFFECTS: moves a certain amount from one 
+    // account to another
+    public void transfer() {}
+
+    // EFFECTS: pauses console to let user read
+    public void enter() {
+        System.out.println("Press enter to continue");
+        input.nextLine();
     }
     
 }
