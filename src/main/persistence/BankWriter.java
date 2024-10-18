77b565d06bda76256d726cd3d7df0c86bc4c01a4
Anthony Sheu <asheu@students.cs.ubc.ca>
2024-10-18 15:24:43 -0700

    Added implementation for BankWriter

--- src/main/persistence/BankWriter.java
+++ src/main/persistence/BankWriter.java
@@ -1,7 +1,11 @@
 package persistence;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import model.Banks;
+import model.Account;
+
+import java.util.*;
 
 public class BankWriter extends JsonWriter {
 
@@ -13,7 +17,33 @@ public class BankWriter extends JsonWriter {
     // EFFECTS: returns a JSONObject of the data that
     // is going to be written
     public JSONObject toJson(Banks bank) {
-        return null;
+        JSONObject json = new JSONObject();
+        json.put("Accounts", accountsToJson(bank.getBank()));
+        return json;
+    }
+
+    // EFFECTS: returns accounts in JSON array
+    public JSONArray accountsToJson(List<Account> accounts) {
+        JSONArray jsonArray = new JSONArray();
+        for (Account a : accounts) {
+            jsonArray.put(accountToJson(a));
+        }
+        return jsonArray;
+    }
+
+    // EFFECTS: takes in an account and converts it into 
+    // a JSON object
+    public JSONObject accountToJson(Account account) {
+        JSONObject json = new JSONObject();
+        json.put("Bank", account.getBank());
+        json.put("Chequeing", account.getChequeing());
+        json.put("Savings", account.getSavings());
+        json.put("Credit", account.getCredit());
+        json.put("Credit Limit", account.getCreditLimit());
+        json.put("Chequeing Overdraft", account.checkOverdraftChequeing());
+        json.put("Savings Overdraft", account.checkOverdraftSavings());
+        json.put("Credit Overusage", account.checkOverLimit());
+        return json;
     }
 
 }
---
+++ src/test/persistence/CategoriesIOTest.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import model.Categories;
+import model.Expense;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.jar.JarEntry;
+
+public class CategoriesIOTest {
+
+}
