e4e9f300a24ff096d6761e87c9a6cd9ef10510af
Anthony Sheu <asheu@students.cs.ubc.ca>
2024-10-19 15:40:38 -0700

    Fixed a bug that was causing tests to fail

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -3,13 +3,16 @@ package persistence;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import netscape.javascript.JSException;
+
 import java.io.IOException;
 import java.util.jar.JarEntry;
 
 public class JsonReaderTest {
 
     @Test
-    protected void testReaderNonExistentFile() {
+    public void testReaderNonExistentFile() {
         JsonReader reader = new JsonReader("./data/rainbow.json");
         try {
             reader.read();
@@ -19,5 +22,22 @@ public class JsonReaderTest {
         }
     }
 
+    @Test
+    public void testReadEmptyFile() {
+        JsonReader reader = new JsonReader("./data/Empty.json");
+        assertFalse(reader.checkFile());
+    }
+
+    @Test
+    public void testReadFile() {
+        JsonReader reader = new JsonReader("./data/CorrectFile.json");
+        assertTrue(reader.checkFile());
+    }
+
+    @Test
+    public void testReadErrorFile() {
+        JsonReader reader = new JsonReader("./data/ErrorFil.json");
+        assertFalse(reader.checkFile());
+    }
 
 }
